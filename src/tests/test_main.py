c9dcd8b9ef6a55248750b05986b953b0b470d7ed
juanjtov <juanjosetmontana@gmail.com>
2021-09-04 15:22:40 -0700

    test app added as a fixture

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -1,15 +1,16 @@
 from starlette.testclient import TestClient
 
-from app.main import app
-
-client = TestClient(app)
-
-def test_ping():
-    response = client.get("/ping")
+def test_ping(test_app):
+    response = test_app.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!"}
 
-def test_landing():
-    response = client.get("/")
+def test_landing(test_app):
+    response = test_app.get("/")
     assert response.status_code == 200
     assert response.json() == {"title": "Backend API for the wedding project"}
+
+# def test_login():
+#     response = cliend.post("/login")
+#     assert response.status_code == 200
+    # assert response.json() == 
