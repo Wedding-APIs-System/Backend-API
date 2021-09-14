854106c9599f39d40f31b8c555b258a6c70b5e10
juanjtov <juanjosetmontana@gmail.com>
2021-09-14 13:06:39 -0700

    Tests working

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -10,6 +10,36 @@ def test_landing(test_app):
     assert response.status_code == 200
     assert response.json() == {"title": "Backend API for the wedding project"}
 
+def test_login_existing(test_app):
+    response = test_app.get("/login/3002022460")
+    assert response.status_code == 200
+    assert response.json() == {'attendance_confirmation': 'True', 'family_name': 'Familia Tovar Uribe'} 
+
+def test_login_unexisting(test_app):
+    response = test_app.get("/login/6692958490")
+    assert response.status_code == 404
+    assert response.json() == {'detail': 'User not found'}  
+
+def test_attendance_confirmation(test_app):
+    response = test_app.put(
+        "/confirmation/3002022460",
+        
+        json={
+                "attendance_confirmation": "true"
+        }
+    )
+    assert response.status_code == 200
+    assert response.json() == {
+        "guest_id": 21,
+        "family_id": 11,
+        "name": "Juan Jose Tovar",
+        "phone_number": "3002022460",
+        "attendance_confirmation": 1,
+        "allergies": "",
+        "additional_comments": ""
+    }
+
+
 # def test_login():
 #     response = cliend.post("/login")
 #     assert response.status_code == 200
